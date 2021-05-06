2f4b0fac95e7a3aa904db62fbded8fdecf29479f
dinirichard <dinirichard@hotmail.com>
2021-05-06 13:07:34 +0300

    Created flixtor flip animation

--- src/app/dd-transforms/dd-transforms.component.ts
+++ src/app/dd-transforms/dd-transforms.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-dd-transforms',
-  templateUrl: './dd-transforms.component.html',
-  styleUrls: ['./dd-transforms.component.css']
+  selector: "app-dd-transforms",
+  templateUrl: "./dd-transforms.component.html",
+  styleUrls: ["./dd-transforms.component.scss"]
 })
 export class DDTransformsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
